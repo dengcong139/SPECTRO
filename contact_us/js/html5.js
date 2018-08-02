eb50ea36a31da3d9c93389114b550477999affd6
dengcong139 <36321743+dengcong139@users.noreply.github.com>
2018-08-02 19:42:27 +0800

    Initial commit

---
+++ contact_us/js/html5.js
@@ -0,0 +1,5 @@
+// JavaScript Document
+ 
+ (function(){if(!/*@cc_on!@*/0)return;  var e = "abbr,article,aside,audio,main，canvas,datalist,details,dialog,eventsource,figure,footer,header,hgroup,mark,menu,meter,nav,output,progress,section,time,video".split(','),i=e.length;while(i--){document.createElement(e[i]); }})()
+ 
+       
